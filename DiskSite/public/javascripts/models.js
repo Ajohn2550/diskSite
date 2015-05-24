8637848f5606176dba2661f67890f545bde8726e
Alexander Johnson <Bots@JohnsonWebWare.com>
2015-05-24 15:29:04 -0500

    started on models.

---
+++ DiskSite/public/javascripts/models.js
@@ -0,0 +1,33 @@
+﻿var _debug = {
+  active: true,
+  log: [],
+  logger: function (data) {
+    if (this.active) this.log.push(data);
+  }
+};
+
+var Player = function (firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.fullName = firstName + ' ' + lastName;
+  _debug.logger('Added Player: ' + this.fullName);
+};
+var Hole = function (number, score, par) {
+  this.number = number;
+  this.par = par;
+  this
+};
+var Course = function (numHoles, name, geo, address) {
+  this.name = name;
+  this.geo = geo || null;
+  this.address = address || null;
+};
+var Team   = Team   || {};
+
+var Players = [];
+
+Players.push(new Player('Alex', 'Johnson'));
+
+
+
+if (_debug.active) console.log(_debug.log);
